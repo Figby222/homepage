4ffd970a438c246c20d66cd5ccf94a95f0625811
Figby222 <figby222@outlook.com>
2024-04-27 05:57:08 -0700

    Import all images & fix style import path

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-import "./styles.css";
+import "../src/styles.css";
+import "../src/assets/icons/github-original-wordmark.svg";
+import "../src/assets/icons/open-in-new.svg";
+import "../src/assets/images/placeholder-project.png";
+import "../src/assets/images/placeholder-portrait-800w.jpg";
+import "../src/assets/images/placeholder-portrait.jpg"
+import "../src/assets/images/placeholder-portrait-480w.jpg";
