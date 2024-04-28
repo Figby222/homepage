1fbe348d7cc2b734e841b06e984bf50a77bc50a9
Figby222 <figby222@outlook.com>
2024-04-28 01:13:54 -0700

    Add html-loader & load html

--- webpack.common.cjs
+++ webpack.common.cjs
@@ -14,6 +14,10 @@ module.exports = {
   module: {
     rules: [
         {
+            test: /\.(html)$/,
+            use: ["html-loader"]
+        },
+        {
             test: /\.css$/i,
             use: ["style-loader", "css-loader"],
         },
