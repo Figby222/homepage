d7a393cd4569ecaa364b2716370c16f259e8e7a6
Figby222 <figby222@outlook.com>
2024-04-28 00:12:38 -0700

    Import linked-in icon

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-import "../src/styles.css";
-import "../src/assets/icons/github-original-wordmark.svg";
-import "../src/assets/icons/open-in-new.svg";
-import "../src/assets/images/placeholder-project.png";
-import "../src/assets/images/placeholder-portrait-800w.jpg";
-import "../src/assets/images/placeholder-portrait.jpg"
-import "../src/assets/images/placeholder-portrait-480w.jpg";
+import "./styles.css";
+import "./assets/icons/linkedin-original-wordmark.svg";
+import "./assets/icons/github-original-wordmark.svg";
+import "./assets/icons/open-in-new.svg";
+import "./assets/images/placeholder-project.png";
+import "./assets/images/placeholder-portrait-800w.jpg";
+import "./assets/images/placeholder-portrait.jpg"
+import "./assets/images/placeholder-portrait-480w.jpg";
