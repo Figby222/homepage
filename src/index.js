54b638264fc54095348a91fee4b6bdd18c720775
Figby222 <figby222@outlook.com>
2024-04-28 01:16:32 -0700

    Remove image imports

--- src/index.js
+++ src/index.js
@@ -1,8 +1,1 @@
 import "./styles.css";
-import "./assets/icons/linkedin-original-wordmark.svg";
-import "./assets/icons/github-original-wordmark.svg";
-import "./assets/icons/open-in-new.svg";
-import "./assets/images/placeholder-project.png";
-import "./assets/images/placeholder-portrait-800w.jpg";
-import "./assets/images/placeholder-portrait.jpg"
-import "./assets/images/placeholder-portrait-480w.jpg";
